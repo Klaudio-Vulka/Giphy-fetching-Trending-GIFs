56bb653513a3e6915af655027f1037a18ab98f88
Klaudio-Vulka <160673170+Klaudio-Vulka@users.noreply.github.com>
2024-10-09 09:54:34 -0400

    Add files via upload

---
+++ bestCodeAPI/myBestCode.py
@@ -0,0 +1,13 @@
+import requests
+
+
+API_KEY = "vUxEs6Em2tLnP61YCi1eImSmhziWOp87"
+endpoint = "https://api.giphy.com/v1/gifs/trending"
+
+params = {"api_key" : API_KEY, "limit": 3, "rating": "g"}
+response = requests.get(endpoint, params = params).json()
+for gif in response["data"]:
+    title = gif["title"]
+    trending_date = gif["trending_datetime"]
+    url = gif["url"]
+    print(f"{title} | {trending_date} | {url}")
